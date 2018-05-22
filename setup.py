9daa7a510f80759c407eb9664237a935dd0442b8
MrGreenTea <mrlordalfred@gmail.com>
2018-05-22 15:09:57 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env
+
+from distutils.core import setup
+
+setup(
+    name='docker_secrets',
+    version='0.1',
+    description='Extremely simple module to get docker secrets',
+    author='Jonas Bulik',
+    author_email='mrlordalfred@gmail.com',
+    module=['docker_secrets']
+)
